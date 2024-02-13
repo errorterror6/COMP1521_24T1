ed966f3c032799ab6f9d487ff094e6afbcde2b39
Raydiation <z5360323@ad.unsw.edu.au>
2024-02-13 17:25:26 +1100

    week1

---
+++ week1/q5.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+
+// int main(void) {
+//   int i = 0;
+//   while (i < 10) {
+//     printf("%d\n", i);
+//     i++;
+//   }
+//   return 0;
+// }
+
+#include <stdio.h>
+
+int main(void) {
+  
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", i);
+  }
+  return 0;
+}
